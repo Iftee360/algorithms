4ddc86140f9fd0402d2e57c73b6cbf09082b740f
Iftee360 <ifteeprogrammer360@gmail.com>
2020-08-07 10:22:56 +0600

    fixed typo
    instead of big O (1), it should be Sigma (1)

--- searching-alogrithms/linear-search.c
+++ searching-alogrithms/linear-search.c
@@ -15,7 +15,7 @@
 either because the target element is the last element of the array or doesn't
 exist in the array at all.
 
-*Best-case scenenraio [Big O (1)]: the trget element is the first element of the array,
+*Best-case scenenraio [Sigma (1)]: the trget element is the first element of the array,
 and so we can stop looking immediately after we start
 */
 //CODE (in C language)
